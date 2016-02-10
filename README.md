828e3ace276bc2e70069f4e8834a3ec40f0d438d
Benjamin M. Brown <benjaminbb@gmail.com>
2016-02-10 00:11:39 -0600

    adding client to make some requests

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # zeromq-multiprocessing-pyzmq
 Multiprocessing request/response pattern in ZeroMQ with PyZmq
+
+<br/>
+You can spin up multiple instances of servers and clients.
+
+<br/>
+Just run python zmq-req-rep-multiprocessing.py
--- zmq-req-rep-multiprocessing.py
+++ zmq-req-rep-multiprocessing.py
@@ -14,7 +14,22 @@ def server(port="5556"):
 		print "Got request  %s" % message
 		socket.send("We got your request!")
 
+def client(ports=["5556"]):
+	context = zmq.Context()
+	print "Connecting to server on ports %s" % ports
+	socket = context.socket(zmq.REQ)
+	for port in ports:
+		socket.connect("tcp://localhost:%s" %port)
+	for request in range(50):
+		print "Sending client request", request, "..."
+		socket.send("Hello")
+		message = socket.recv()
+		print "Got a reply", request, "[", message,"]", port
+		time.sleep(0.5)
+
 if __name__ == "__main__":
-	server_ports = range(5540,5558, 4)
+	server_ports = range(5540,5558, 1)
 	for server_port in server_ports:
 		Process(target=server, args=(server_port,)).start()
+
+	Process(target=client, args=(server_ports,)).start()
